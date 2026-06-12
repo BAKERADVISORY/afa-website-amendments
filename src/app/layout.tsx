1277dace7ba5ec26953cb7aac8733765645eeee9
Jason <jason@tradeshq.com.au>
2026-06-12 13:03:17 +1000

    Install GA4 tracking G-B226QNH900

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -175,6 +175,20 @@ export default function RootLayout({
     <html lang="en-AU" className={`${manrope.variable} h-full antialiased`}>
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B226QNH900"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-B226QNH900');
+`,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
